a25c7c5ff5542f628316b164f24d972f11429a7b
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-25 13:36:32 +0530

    (Samiul Mamud) created: Cab invoice service System is created

---
+++ src/main/java/com/capgemini/cabinvoicegenerator/MainClass.java
@@ -0,0 +1,8 @@
+package com.capgemini.cabinvoicegenerator;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the cab Invoice Service");
+	}
+}
---
+++ src/test/java/com/capgemini/cabinvoicetest/TestClass.java
@@ -0,0 +1,8 @@
+package com.capgemini.cabinvoicetest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestClass {
+
+}
